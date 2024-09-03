3d1f6610173497ad2160e23e3599379ad53e752d
MANGU RAM HEMBRAM <rntc0822006@nttf.co.in>
2024-09-03 10:59:28 +0530

    new

--- SEM Inheritance.py
+++ SEM Inheritance.py
@@ -3,7 +3,7 @@ class animal:
         print("animal_speaking")
 class cat(animal):
     def miaw(self):
-        print("cat crying")
+        print("cat crying like a PUSSY")
 c=cat()
 c.speak()
 c.miaw()
